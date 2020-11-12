3b3f27ec858aaae379a7fd198b032cdaa543bd56
Aleix Pol <aleixpol@kde.org>
2020-11-13 00:51:20 +0100

    Split AbstractClient::iconFromDesktopFile into a static function
    This way it can be used before we have the window

--- src/abstract_client.cpp
+++ src/abstract_client.cpp
@@ -2658,11 +2658,15 @@ void AbstractClient::setDesktopFileName(QByteArray name)
 
 QString AbstractClient::iconFromDesktopFile() const
 {
-    if (m_desktopFileName.isEmpty()) {
+    return iconFromDesktopFile(QFile::decodeName(m_desktopFileName));
+}
+
+QString AbstractClient::iconFromDesktopFile(const QString &desktopFileName)
+{
+    if (desktopFileName.isEmpty()) {
         return {};
     }
 
-    const QString desktopFileName = QString::fromUtf8(m_desktopFileName);
     QString desktopFilePath;
 
     if (QDir::isAbsolutePath(desktopFileName)) {
@@ -2676,6 +2680,9 @@ QString AbstractClient::iconFromDesktopFile() const
     if (desktopFilePath.isEmpty()) {
         desktopFilePath = QStandardPaths::locate(QStandardPaths::ApplicationsLocation,
                                                  desktopFileName + QLatin1String(".desktop"));
+    }
+    if (desktopFilePath.isEmpty()) {
+        return {};
     }
 
     KDesktopFile df(desktopFilePath);
