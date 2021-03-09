dingjingmaster/kwin

Refactor session code
At the moment, the session code is far from being extensible. If we
decide to add support for libseatd, it will be a challenging task with
the current design of session management code. The goal of this
refactoring is to fix that.

Another motivation behind this change is to prepare session related code
for upstreaming to kwayland-server where it belongs.

session_noop.cpp
@@ -1,0 +1,57 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "session_noop_p.h"
+
+namespace KWin
+{
+
+NoopSession *NoopSession::create(QObject *parent)
+{
+    return new NoopSession(parent);
+}
+
+NoopSession::NoopSession(QObject *parent)
+    : Session(parent)
+{
+}
+
+NoopSession::~NoopSession()
+{
+}
+
+bool NoopSession::isActive() const
+{
+    return true;
+}
+
+QString NoopSession::seat() const
+{
+    return QStringLiteral("seat0");
+}
+
+uint NoopSession::terminal() const
+{
+    return 0;
+}
+
+int NoopSession::openRestricted(const QString &fileName)
+{
+    Q_UNUSED(fileName)
+    return -1;
+}
+
+void NoopSession::closeRestricted(int fileDescriptor)
+{
+    Q_UNUSED(fileDescriptor)
+}
+
+void NoopSession::switchTo(uint terminal)
+{
+    Q_UNUSED(terminal)
+}
+
+} // namespace KWin

feren-OS/kwin-patching

src/session_noop.cpp
@@ -1,0 +1,57 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "session_noop.h"
+
+namespace KWin
+{
+
+NoopSession *NoopSession::create(QObject *parent)
+{
+    return new NoopSession(parent);
+}
+
+NoopSession::NoopSession(QObject *parent)
+    : Session(parent)
+{
+}
+
+NoopSession::~NoopSession()
+{
+}
+
+bool NoopSession::isActive() const
+{
+    return true;
+}
+
+QString NoopSession::seat() const
+{
+    return QStringLiteral("seat0");
+}
+
+uint NoopSession::terminal() const
+{
+    return 0;
+}
+
+int NoopSession::openRestricted(const QString &fileName)
+{
+    Q_UNUSED(fileName)
+    return -1;
+}
+
+void NoopSession::closeRestricted(int fileDescriptor)
+{
+    Q_UNUSED(fileDescriptor)
+}
+
+void NoopSession::switchTo(uint terminal)
+{
+    Q_UNUSED(terminal)
+}
+
+} // namespace KWin